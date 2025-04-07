f65807c2dbfe40a7495de4e5c76feb4354dbb96d
abbas <abbasbukhari2015@gmail.com>
2025-04-06 23:16:34 -0400

    Initial commit: Set up backend and frontend for PhotoLabs

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,9 +1,8 @@
-const PORT = process.env.PORT || 8001;
+const PORT = 8001;
 const ENV = require("./environment");
 
 const app = require("./application")(ENV);
-const server = require("http").Server(app);
 
-server.listen(PORT, () => {
+app.listen(PORT, () => {
   console.log(`Listening on port ${PORT} in ${ENV} mode.`);
 });
